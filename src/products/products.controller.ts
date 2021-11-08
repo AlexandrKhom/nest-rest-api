ed9d4c28ae0caa3801e32804236f94099a752c1a
AlexandrKhom <alex988andr@gmail.com>
2021-11-09 00:06:42 +0200

    3_DTO

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -6,8 +16,24 @@ export class ProductsController {
   getAll(): string {
     return 'getAll';
   }
+
   @Get(':id')
   getOne(@Param('id') id: string): string {
     return 'getOne ' + id;
+  }
+
+  @Post()
+  create(@Body() createProductDto: CreateProductDto) {
+    return `Title: ${createProductDto.title} Price: ${createProductDto.price}`;
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): string {
+    return 'Delete ' + id;
+  }
+
+  @Put(':id')
+  update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string) {
+    return `Update ` + id;
   }
 }
