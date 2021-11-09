a7ef42cd42ce63f96ca423e7f899f6e5e84663d2
AlexandrKhom <alex988andr@gmail.com>
2021-11-09 03:04:09 +0200

    7_mongo

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -16,6 +16,7 @@ import {
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { ProductsService } from './products.service';
+import { Product } from './schemas/product.schema';
 
 @Controller('products')
 export class ProductsController {
@@ -29,29 +30,32 @@ export class ProductsController {
   // }
 
   @Get()
-  getAll() {
+  getAll(): Promise<Product[]> {
     return this.productsService.getAll();
   }
 
   @Get(':id')
-  getOne(@Param('id') id: string) {
+  getOne(@Param('id') id: string): Promise<Product> {
     return this.productsService.getById(id);
   }
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
   @Header('Cache-Control', 'none')
-  create(@Body() createProductDto: CreateProductDto) {
+  create(@Body() createProductDto: CreateProductDto): Promise<Product> {
     return this.productsService.create(createProductDto);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: string): string {
-    return 'Delete ' + id;
+  remove(@Param('id') id: string): Promise<Product> {
+    return this.productsService.remove(id);
   }
 
   @Put(':id')
-  update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string) {
-    return `Update ` + id;
+  update(
+    @Body() updateProductDto: UpdateProductDto,
+    @Param('id') id: string,
+  ): Promise<Product> {
+    return this.productsService.update(id, updateProductDto);
   }
 }
